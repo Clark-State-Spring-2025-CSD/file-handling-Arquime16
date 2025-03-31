7f23f60c7b36af6a6e44ccee301b6b06b4c889fd
Arquimedes <105887093+Arquime16@users.noreply.github.com>
2025-03-31 18:34:02 -0500

    Print Document TXT Assesment

--- fileecho.py
+++ fileecho.py
@@ -1,3 +1,19 @@
 #Read in the echo.txt file and display it in the terminal using print()
 #This is a guided practice. Either follow the video or your instructor in class.
 
+#Open the file in read mode
+file = open("echo.txt", "r")
+#Read the contents of the file
+
+contents = file.read()
+#Close the file 
+file.close()
+#Display the contents of the file in the terminal
+
+print(contents)
+#This is a guided practice. Either follow the video or your instructor in class.
+
+
+
+
+

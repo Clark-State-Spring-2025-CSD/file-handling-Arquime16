bc0e80ab543ffcd760f1b5ad83329f5c6e62f237
Arquimedes <105887093+Arquime16@users.noreply.github.com>
2025-04-04 16:35:42 -0500

    Update highlowaverage.py

--- highlowaverage.py
+++ highlowaverage.py
@@ -7,3 +7,36 @@
 #Highest number
 #Lowest number
 #Correct answers for the included file:
+
+#How many numbers in the file
+
+
+count = 0
+
+file = open("numbers.txt", "r")
+for line in file:
+    for eachNumbers in line.split():
+        if eachNumbers.isnumeric():
+            count += 1
+
+file.close()          
+print(f"Total numbers:", count)
+
+total_sum = 0
+
+total = open("numbers.txt", "r")
+for line in total:
+    for numbers in line.split():
+        if numbers.isnumeric():
+            total_sum += int(numbers)
+
+total.close()
+print("Total sum of numbers:", total_sum)
+
+
+
+
+
+
+
+

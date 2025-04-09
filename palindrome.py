1685df514bf38c9b3e5da20238f29edfb5a5344c
Arquimedes <105887093+Arquime16@users.noreply.github.com>
2025-04-09 16:48:51 -0500

    Update palindrome.py
    it is done. Thanks!

--- palindrome.py
+++ palindrome.py
@@ -3,3 +3,25 @@
 #For example, wow is a paldindrome.
 #A different file wile be used for grading.
 #Correct answer for this file: 
+
+file = open("words.txt")
+
+count = 0
+
+for line in file.readlines():
+  line = line.strip()
+  if line == ''.join(reversed(line)):
+    count += 1
+    print(f"{line}")
+    
+print(f"{count}")
+
+    
+ 
+    
+file.close()
+    
+  
+
+
+ 
